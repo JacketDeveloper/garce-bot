3b348cce07f5c810756a0ee2d3dc7b2140563ebe
Jacket_Osint <90003462+JacketDeveloper@users.noreply.github.com>
2022-05-02 15:24:53 -1100

    Add files via upload

---
+++ result/game/riddle-es.js
@@ -0,0 +1,27 @@
+[ 
+{ "author" : "garce",
+    "index": 1,
+    "soal": "Animales como las lagartijas que pueden cambiar el color de su piel.?",
+    "jawaban": "Camaleón"
+  },
+  { "author" : "jacket",
+    "index": 2,
+    "soal": "Carta de instrucción a Suharto para hacerse cargo de la seguridad de Indonesia el 11 de marzo de 1966?",
+    "jawaban": "Supersemar"
+  },
+  { "author" : "jacket",
+    "index": 3,
+    "soal": "El río más largo de América?",
+    "jawaban": "Amazon"
+  },
+  { "author" : "garce",
+    "index": 6,
+    "soal": "¿El nombre de la película que cuenta la historia de un robot que puede convertirse en vehículo?",
+    "jawaban": "Transformers"
+  },
+  { "author" : "garce",
+    "index": 7,
+    "soal": "¿El nombre del lujoso barco de pasajeros que se hundió durante su viaje inaugural en 1912?",
+    "jawaban": "Titanic"
+  }
+] 
