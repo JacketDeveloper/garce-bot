6b8f2372625ed39a0fad52a91ab03de99e71b1bd
Jacket_Osint <90003462+JacketDeveloper@users.noreply.github.com>
2022-05-14 07:24:05 -0600

    Update start.js

--- start.js
+++ start.js
@@ -5,12 +5,12 @@ let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
 
-CFonts.say('FG98', {
+CFonts.say('Jacket', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`dylux-bot By FG98 Ig : @fg98._`, {
+CFonts.say(`Garce-bot By Jacket`, {
   font: 'console',
   align: 'center',
   colors: ['yellow']
